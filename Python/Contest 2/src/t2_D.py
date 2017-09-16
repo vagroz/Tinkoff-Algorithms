3c98dcbbbea5300c40ccb91e18bc9685292d8f75
Ivan Malyshev <vanya-grozny@yandex.ru>
2017-09-16 23:28:39 +0300

    beta version of problem D

---
+++ Python/Contest 2/src/t2_D.py
@@ -0,0 +1,28 @@
+fout = open ("ropes.out", "w")
+fin = open ("ropes.in", "r")
+
+(n, k) = map(lambda x: int(x), fin.readline().split())
+mas = []
+left=0
+right=0
+for i in range (n):
+    l = int(fin.readline())
+    if l>right:
+        right=l
+    mas.append(l)
+
+def count(length):
+    summ = 0
+    for l in mas:
+        summ+= l/length
+    return summ
+
+while left+1<right:
+    mid = (left+right)/2
+    if count(mid)>=k:
+        left=mid
+    else:
+        right=mid
+fout.write(str(left))
+
+    
