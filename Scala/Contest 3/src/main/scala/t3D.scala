ca1474120f46796421486fea178323b68273a3e0
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-10-02 01:24:29 +0300

    Problem D beta

---
+++ Scala/Contest 3/src/main/scala/t3D.scala
@@ -0,0 +1,62 @@
+import scala.io.StdIn
+import scala.collection.immutable.Vector
+import scala.math._
+
+object t3D extends App {
+  val inpt = StdIn.readLine.split(" ").map(_.toInt)
+  val N = inpt(0);
+  val M = inpt(1)
+  val olenenok1 = Array.ofDim[Int](N, M)
+  for (i <- 0 until N) {
+    olenenok1(i) = StdIn.readLine.split(" ") map (_.toInt)
+  }
+  val dp = olenenok1.clone()
+  for (i <- 0 until N) {
+    dp(i) = dp(i).map(_ => Int.MaxValue)
+  }
+
+  val olenenok = olenenok1.toVector.map(_.toVector)
+  dp(0)(0) = olenenok(0)(0)
+
+  def app(x: Int, y: Int, value: Int): Unit = {
+    if (x >= 0 && y >= 0 && x < N && y < M) {
+      dp(x)(y) = min(value + olenenok(x)(y), dp(x)(y))
+    }
+  }
+
+  def func(x: Int, y: Int): Unit = {
+    if (x < 0 || y >= M) {
+      val n = x + y
+      func(n + 1, 0)
+    } else if (x >= N) {
+      func(N - 1, y + x - N)
+    } else {
+      val a = x + 1
+      val b = y + 1
+      val value = dp(x)(y)
+      app(x, b, value)
+      app(a, y, value)
+      if (x != N - 1 && y != M - 1)
+        func(x - 1, y + 1)
+    }
+  }
+
+  func(0, 0)
+  println(dp(N - 1)(M - 1))
+
+  val n = N + M - 2
+  println(n+1)
+
+  var xx = N - 1
+  var yy = M - 1
+  val way = for (i <- 1 to n) yield {
+    val geo = (xx+1, yy+1)
+    if (dp(xx - 1)(yy) == dp(xx)(yy) - olenenok(xx)(yy))
+      xx -= 1
+    else
+      yy -= 1
+    geo
+  }
+  {way :+ (1, 1)}.reverse foreach(println)
+
+}
