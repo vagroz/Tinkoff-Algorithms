3bbe405e77b59f77405c562bfe560417fe349f69
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-10-14 20:56:49 +0300

    Task B beta

---
+++ Java/contest4/src/TaskB.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class TaskB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] masStr = br.readLine().split(" ");
+        int[] mas = new int[masStr.length];
+        for (int i = 0; i < masStr.length; i++) {
+            mas[i] = Integer.parseInt(masStr[i]);
+        }
+
+        int[] dp = new int[mas.length];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if ((mas[i] % mas[j] == 0) && (dp[j] + 1 > dp[i])) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        System.out.println(dp[dp.length-1]);
+    }
+}
